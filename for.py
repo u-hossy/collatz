906227a11e2aaa1d8b2a2c118feffa1759cb69cf
u_hossy <95125392+u-hossy@users.noreply.github.com>
2023-10-17 10:57:28 +0900

    繰り返して出力するfor.pyを追加

---
+++ for.py
@@ -0,0 +1,19 @@
+def collatz(num):
+    if num % 2 == 0:
+        return num // 2
+    else:
+        return 3 * num + 1
+
+
+for num in range(1, 1000000):
+    init = open('outputs/' + str(num) + '.csv', 'x')
+    init.write(str(num))
+    filename = num
+    init.close()
+
+    while num != 1:
+        num = collatz(num)
+        print(num)
+        out = open('outputs/' + str(filename) + '.csv', 'a')
+        out.write(',\n' + str(num))
+        out.close()
